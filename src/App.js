0b4c2f8ac213e614720c429eb9721d280c42d1a9
ankitbhusal <ankitbhusal95@gmail.com>
2023-09-20 19:54:06 +0545

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <p>Welcome to react course</p>
+      </div>
+    )
+  }
+}
+
+export default App;
